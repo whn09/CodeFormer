c71d57b75dd622f6f113640126626c83d619197d
EC2 Default User <ec2-user@ip-172-16-40-242.ec2.internal>
2024-05-21 08:20:58 +0000

    optimize memory usage

--- inference_codeformer.py
+++ inference_codeformer.py
@@ -251,13 +251,33 @@ if __name__ == '__main__':
             save_restore_path = os.path.join(result_root, 'final_results', f'{basename}.png')
             imwrite(restored_img, save_restore_path)
 
-    # save enhanced video
+#     # save enhanced video
+#     if input_video:
+#         print('Video Saving...')
+#         # load images
+#         video_frames = []
+#         img_list = sorted(glob.glob(os.path.join(result_root, 'final_results', '*.[jp][pn]g')))
+#         for img_path in img_list:
+#             img = cv2.imread(img_path)
+#             video_frames.append(img)
+#         # write images to video
+#         height, width = video_frames[0].shape[:2]
+#         if args.suffix is not None:
+#             video_name = f'{video_name}_{args.suffix}.png'
+#         save_restore_path = os.path.join(result_root, f'{video_name}.mp4')
+#         vidwriter = VideoWriter(save_restore_path, height, width, fps, audio)
+         
+#         for f in video_frames:
+#             vidwriter.write_frame(f)
+#         vidwriter.close()
+        
+    # save enhanced video TODO optimize memory usage
     if input_video:
         print('Video Saving...')
         # load images
         video_frames = []
         img_list = sorted(glob.glob(os.path.join(result_root, 'final_results', '*.[jp][pn]g')))
-        for img_path in img_list:
+        for img_path in img_list[:1]:
             img = cv2.imread(img_path)
             video_frames.append(img)
         # write images to video
@@ -266,8 +286,9 @@ if __name__ == '__main__':
             video_name = f'{video_name}_{args.suffix}.png'
         save_restore_path = os.path.join(result_root, f'{video_name}.mp4')
         vidwriter = VideoWriter(save_restore_path, height, width, fps, audio)
-         
-        for f in video_frames:
+
+        for img_path in img_list:
+            f = cv2.imread(img_path)
             vidwriter.write_frame(f)
         vidwriter.close()
 
